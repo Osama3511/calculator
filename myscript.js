10af05b07f077eb4799472e80cbd2ce3776ccd69
mohamad osama tarakji <hmmosamu@gmail.com>
2024-10-05 19:44:05 +0300

    close to finishing

--- myscript.js
+++ myscript.js
@@ -1,6 +1,10 @@
 
-let firstOperand = secondOperand = firstOperator = secondOperator = null;
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
 let displayValue = '0';
+let result = 0;
 
 // operation functions
 
@@ -17,7 +21,10 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-    return Number((num1 / num2)).toFixed(5);
+    if(num2 === 0){
+        return "ERROR";
+    }
+    return ((num1 / num2)).toFixed(5);
 }
 
 // updates the display to show displayValue each time we change it
@@ -35,7 +42,7 @@ updateDisplay();
 
 const buttons = document.querySelectorAll("button");
 
-
+// goes through each button click
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         if(button.classList.contains("operand")){
@@ -53,17 +60,18 @@ buttons.forEach(button => {
             equals();
             updateDisplay();
         }
-        
     });
 });
 
 
 
 function inputOperand(operand){
+    // inputs to firstOperand by check if there is no operator clicked
     if(firstOperator == null){
-        if(displayValue === '0'){
+        if(displayValue === '0' || displayValue == 0){
             displayValue = operand;
         }
+        // if displayValue equals to firstOperand that means its a result and resets it 
         else if(displayValue === firstOperand){
             displayValue = operand;
         }
@@ -71,7 +79,9 @@ function inputOperand(operand){
             displayValue += operand;
         }
     }
+    // inputs to secondOperand
     else{
+        // same expression in line 74
         if(displayValue === firstOperand){
             displayValue = operand;
         }
@@ -82,7 +92,15 @@ function inputOperand(operand){
 }
 
 function inputOperator(operator){
-    if(firstOperator == null){
+    if(firstOperator != null && secondOperator == null){
+        secondOperator = operator;
+        equals();
+        updateDisplay();
+    }
+    else if(firstOperator != null && secondOperator != null){
+        let doddo = 13;
+    }
+    else{
         firstOperator = operator;
         firstOperand = displayValue;
     }
@@ -96,15 +114,27 @@ function clearDisplay(){
 }
 
 function equals(){
-    if(firstOperand == null){
-        updateDisplay();
+    if(firstOperator === null){
+        displayValue = displayValue
+    }
+    else if(secondOperator != null){
+        secondOperand = displayValue;
+        result = caclulate(Number(firstOperand),Number(secondOperand),secondOperator);
+        displayValue = result.toString();
+        firstOperand = displayValue;
+        firstOperator = null;
+        secondOperand = null;
+        secondOperator = null;
+        result = null;
     }
     else{
         secondOperand = displayValue;
         result = caclulate(Number(firstOperand),Number(secondOperand),firstOperator);
-        displayValue = result;
+        displayValue = result.toString();
         firstOperand = displayValue;
-        updateDisplay();
+        firstOperator = null;
+        secondOperand = null;
+        result = null;
     }
 }
 
@@ -119,12 +149,8 @@ function caclulate(num1, num2, op){
     else if(op === '*'){
         return multiply(num1 ,num2);
     }
-    else{
+    else if(op === '/'){
         return divide(num1, num2);
     }
 }
 
-// function clear(){
-//     displayValue = "";
-//     updateDisplay();
-// }
